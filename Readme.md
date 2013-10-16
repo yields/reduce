fca1160a2d31b1421b41230f7cd813d750e45104
Amir Abu Shareb <yields@icloud.com>
2013-10-16 17:10:00 +0300

    Release 0.0.1

--- Readme.md
+++ Readme.md
@@ -9,9 +9,13 @@
 
     $ component install yields/reduce
 
-## API
+## Example
 
-
+```js
+assert(3 == reduce({ a: 1, b: 2 }, sum, 0));
+assert(3 == reduce([1, 2], sum, 0));
+assert(198 == reduce('abc', sum, 0));
+```
 
 ## License
 
--- test/test.js
+++ test/test.js
@@ -5,22 +5,22 @@ describe('reduce', function(){
     , reduce = require('reduce');
 
   it('object', function(){
-    assert(3 == reduce({ a: 1, b: 2 }, add, 0));
-    function add(n, k, v){
+    assert(3 == reduce({ a: 1, b: 2 }, sum, 0));
+    function sum(n, k, v){
       return n += v;
     }
   })
 
   it('array', function(){
-    assert(3 == reduce([1, 2], add, 0));
-    function add(n, v){
+    assert(3 == reduce([1, 2], sum, 0));
+    function sum(n, v){
       return n += v;
     }
   })
 
   it('string', function(){
-    assert(198 == reduce('abc', add, 0));
-    function add(n, v){
+    assert(198 == reduce('abc', sum, 0));
+    function sum(n, v){
       return n += v
         .toUpperCase()
         .charCodeAt(0);
